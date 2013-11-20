63b6852bb07fdc810d034126983f480ad41ca853
scottpang <scottpang@yahoo.com>
2013-11-20 09:09:57 +0800

    sdc

--- 购运动/CategoryDetailViewController.m
+++ 购运动/CategoryDetailViewController.m
@@ -22,6 +22,7 @@
     {
         _cate = [cate copy];
     }
+    return self;
 }
 
 - (void)viewDidLoad
@@ -29,7 +30,38 @@
     [super viewDidLoad];
     /*加载数据*/
     [[STDataHelper sharedInstance] homeLoadCategoryDetail:_cate];
-    [NSNotificationCenter defaultCenter] addObserver:<#(id)#> selector:<#(SEL)#> name:<#(NSString *)#> object:<#(id)#>
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loadCategoryDetailCompleted) name:kNotifyHomeLoadCategoryDetailCompleted object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loadCategoryDetailFailed) name:kNotifyHomeLoadCategoryDetailFailed object:nil];
+}
+
+#pragma mark - 下载回调
+- (void)loadCategoryDetailCompleted
+{
+    //            NSMutableArray *results = [[NSMutableArray alloc] init];
+    //            for (NSDictionary *dict in list) {
+    //                NSString *itemName = [dict objectForKey:@"name"];
+    //                NSString *itemFavs = [dict objectForKey:@"favs"];
+    //                NSString *itemPreviewUrl = [dict objectForKey:@"previewUrl"];
+    //                NSString *itemPrice = [dict objectForKey:@"price"];
+    //                NSString *itemUrl = [dict objectForKey:@"url"];
+    //                NSString *itemAct = [dict objectForKey:@"act"];
+    //                NSString *itemId = [dict objectForKey:@"ID"];
+    //
+    //                STModelItem *item = [[STModelItem alloc] init];
+    //                item.itemName = itemName;
+    //                item.itemFavs = itemFavs;
+    //                item.itemPreviewUrl = itemPreviewUrl;
+    //                item.itemPrice = itemPrice;
+    //                item.itemUrl = itemUrl;
+    //                item.itemAct = itemAct;
+    //                item.itemId = itemId;
+    //                [results addObject:item];
+    //            }
+}
+
+- (void)loadCategoryDetailFailed
+{
+    
 }
 
 @end
